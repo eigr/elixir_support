6c6bc348a1c88b120f90a808e898175f62a2abd2
Adriano Santos <sleipnir@bsd.com.br>
2020-10-28 10:51:51 -0300

    Initial Discovery handler

--- apps/cloudstate_elixir_support/lib/cloudstate/discovery/entity_discoveryHandler.ex
+++ apps/cloudstate_elixir_support/lib/cloudstate/discovery/entity_discoveryHandler.ex
@@ -2,7 +2,25 @@ defmodule Cloudstate.EntityDiscoveryHandler do
     use GenServer
     alias Cloudstate.{EntitySpec,ServiceInfo}
 
+    # Client functions
+    def start_link(state) do
+        GenServer.start_link(__MODULE__, state)
+    end
+
+    def discover(pid, proxy_info) do
+        GenServer.call(pid, {:discover, proxy_info})
+    end
+
+    # Server callbacks
+
+    @impl true
     def init(state) do
         {:ok, state}
+    end
+
+    @impl true
+    def handle_call({:discover, proxy_info}, _from, state) do
+        entity_spec = EntitySpec.new(proto: nil, entities: nil, service_info: nil)
+        {:reply, entity_spec, state}
     end
 end
